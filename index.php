0f9f61796c3a33108e07846654eaebe243803c04
Justin <justinmhoch@gmail.com>
2022-02-10 18:00:05 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Your Movie Club</title>
+  </head>
+  <body>
+    <div class="coming-soon">
+      <h1>Your Movie Club</h1>
+      <p>Coming Soon!</p>
+    </div>
+  </body>
+</html>
