a40acd11ebd73d39d629ebd5e3bef7bb3786e8fd
Justin <justinmhoch@gmail.com>
2022-03-02 10:36:44 -0500

    added basic class and database function files

---
+++ private/classes/user.class.php
@@ -0,0 +1,110 @@
+<?php
+
+class User extends DatabaseObject {
+
+  static protected $table_name = "users";
+  static protected $db_columns = ['id', 'username', 'email', 'password', 'user_level', 'avatar_path'];
+
+  public $id;
+  public $username;
+  public $email;
+  protected $password;
+  public $unhashed_password;
+  public $confirm_password;
+  protected $password_required = true;
+  public $user_level;
+  public $avatar_path;
+
+  public function __construct($args=[]) {
+    $this->username = $args['username'] ?? '';
+    $this->email = $args['email'] ?? '';
+    $this->user_level = $args['user_level'] ?? 1;
+    $this->unhashed_password = $args['unhashed_password'] ?? '';
+    $this->confirm_password = $args['confirm_password'] ?? '';
+  }
+
+  protected function set_hashed_password() {
+    $this->password = password_hash($this->unhashed_password, PASSWORD_BCRYPT);
+  }
+
+  public function verify_password($unhashed_password){
+    return password_verify($unhashed_password, $this->password);
+  }
+
+  protected function create() {
+    $this->set_hashed_password();
+    return parent::create();
+  }
+
+  protected function update() {
+    if($this->unhashed_password != '') {
+      // validate password
+      $this->set_hashed_password();
+    } else {
+      // password not being updated, skip hashing and validation
+      $this->password_required = false;
+    }
+    return parent::update();
+  }
+
+  protected function validate() {
+    $this->errors = [];
+  
+    if(is_blank($this->email)) {
+      $this->errors[] = "Email cannot be blank.";
+    } elseif (!has_length($this->email, array('max' => 255))) {
+      $this->errors[] = "Last name must be less than 255 characters.";
+    } elseif (!has_valid_email_format($this->email)) {
+      $this->errors[] = "Email must be a valid format.";
+    }
+  
+    if(is_blank($this->username)) {
+      $this->errors[] = "Username cannot be blank.";
+    } elseif (!has_length($this->username, array('min' => 2, 'max' => 255))) {
+      $this->errors[] = "Username must be between 2 and 255 characters.";
+    } elseif (!has_unique_username($this->username, $this->id ?? 0)) {
+      $this->errors[] = "Username not allowed. Try another.";
+    }
+  
+    if($this->password_required) {
+      if(is_blank($this->unhashed_password)) {
+        $this->errors[] = "Password cannot be blank.";
+      } elseif (!has_length($this->password, array('min' => 4))) {
+        $this->errors[] = "Password must contain 4 or more characters";
+      }
+    
+      if(is_blank($this->confirm_password)) {
+        $this->errors[] = "Confirm password cannot be blank.";
+      } elseif ($this->password !== $this->confirm_password) {
+        $this->errors[] = "Password and confirm password must match.";
+      }
+    }
+  
+    return $this->errors;
+  }
+
+  static public function find_by_username($username) {
+    $sql = "SELECT * FROM " . static::$table_name . " ";
+    $sql .= "WHERE username=" . self::$database->quote($username);
+    $object_array = static::find_by_sql($sql);
+    if(!empty($object_array)) {
+        return array_shift($object_array);
+    }   else    {
+        return false;
+    }
+  }
+
+  static public function find_by_email($email) {
+    $sql = "SELECT * FROM " . static::$table_name . " ";
+    $sql .= "WHERE email=" . self::$database->quote($email);
+    $object_array = static::find_by_sql($sql);
+    if(!empty($object_array)) {
+        return array_shift($object_array);
+    }   else    {
+        return false;
+    }
+  }
+
+}
+
+?>
---
+++ private/initialize.php
@@ -0,0 +1,39 @@
+<?php
+
+  // Turn on output buffering
+  ob_start();
+
+  // Assign file paths to PHP constants
+  // __FILE__ returns the current path to this file
+  // dirname() returns the path to the parent directory
+  define("PRIVATE_PATH", dirname(__FILE__));
+  define("PROJECT_PATH", dirname(PRIVATE_PATH));
+  define("PUBLIC_PATH", PROJECT_PATH);
+  define("SHARED_PATH", PRIVATE_PATH . '/shared');
+
+  // Assign the root URL to a PHP constant
+  define("WWW_ROOT", $_SERVER['SCRIPT_NAME']);
+
+  // Require files
+  require_once('functions.php');
+  require_once('status_error_functions.php');
+  require_once('db_credentials.php');
+  require_once('db_functions.php');
+  require_once('validation_functions.php');
+  
+  // Autoload class definitions
+  function my_autoload($class) {
+    if ( preg_match('/\A\w+\Z/', $class) ) {
+      include 'classes/' . $class . '.class.php';
+    }
+  }
+  spl_autoload_register('my_autoload');
+
+  // Database Connection
+  $database = db_connect();
+  DatabaseObject::set_database($database);
+
+  // Start Session
+  $session = new Session;
+
+?>
